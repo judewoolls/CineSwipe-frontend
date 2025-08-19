767aea20a6edb998a376f585e195aeb48074dfda
Jude Woolls <jwoolls01@gmail.com>
2025-08-20 00:28:44 +0100

    add match cards so the matches display the movie poster and name of movie as well as release year

--- src/components/MatchesList.jsx
+++ src/components/MatchesList.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 
 function MatchesList({ matches }) {
-
   return (
     <div>
-        <ul className="matches-grid">
-          {matches.map((match) => (
-            <li key={match.id} className="match-item">
-              <p>{match.title}</p>
-              <p>{match.description}</p>
-            </li>
-          ))}
-        </ul>
+      <ul className="matches-grid">
+        {matches.map((match) => (
+          <li key={match.id} className="match-item">
+            <img
+              src={
+                match.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${match.poster_path}`
+                  : "https://via.placeholder.com/150"
+              }
+              alt={match.title}
+            />
+            <p><strong>{match.title}</strong> ({match.release_date.split("-")[0]})</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
