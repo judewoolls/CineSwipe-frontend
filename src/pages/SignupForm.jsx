155ab680e3a37d2b9c0f3adedc4de5280771bfd0
Jude Woolls <jwoolls01@gmail.com>
2025-08-20 22:27:39 +0100

    style the sign up form

--- src/pages/SignupForm.jsx
+++ src/pages/SignupForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../css/SignupForm.css";
 
 function SignupPage({ setIsLoggedIn }) {
   const [formData, setFormData] = useState({
