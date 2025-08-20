520b20abcfb4545c84f4d18bf6b91fecef995fed
Jude Woolls <jwoolls01@gmail.com>
2025-08-20 22:33:48 +0100

    add log out button to the nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,21 @@
 import "../css/NavBar.css";
+import LogoutButton from "./LogoutButton";
 
-function NavBar() {
+function NavBar({ setIsLoggedIn }) {
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    setIsLoggedIn(false); // Assuming setIsLoggedIn is passed as a prop
+    
+};
   return (
     <nav className="navbar">
       <p>CineSwipe</p>
       <ul>
-        <li><a href="/home">Home</a></li>
-        <li><a href="/matches">Matches</a></li>
-        <li><a href="/couple">Profile</a></li>
-        <li><a href="/logout">Logout</a></li>
+        <li><a href="/home"><button>Home</button></a></li>
+        <li><a href="/matches"><button>Matches</button></a></li>
+        <li><a href="/couple"><button>Couple</button></a></li>
+        <li><LogoutButton onLogout={handleLogout}/></li>
       </ul>
     </nav>
   );
